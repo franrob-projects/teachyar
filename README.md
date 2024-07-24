62dcd0d3e2ee420ea5ef3ced643506d8f3f40ed3
Francis Roberts <francisroberts@live.com>
2024-07-24 17:18:40 +0100

    Initial commit

--- README.md
+++ README.md
@@ -33,3 +33,4 @@ P.S.
 -   Don't get banned from Stripe for 1 dispute. Use [ByeDispute](https://byedispute.com/?ref=shipfast_readme) to prevent them from happenening 🛡️
 -   Make your launch go viral and get your first customers with [LaunchViral](https://launchvir.al/?ref=shipfast_readme) 🚀
 -   Stop paying 0.4% per Stripe invoices [Zenvoice](https://zenvoice.io/?ref=shipfast_readme) 🤕
+# teachyar
--- app/page.js
+++ app/page.js
@@ -1,43 +1,34 @@
-import Link from "next/link";
-import ButtonSignin from "@/components/ButtonSignin";
+import Header from "@/components/Header";
+import { getSEOTags } from "@/libs/seo";
 
-export default function Page() {
+export const metadata = getSEOTags({ canonicalUrlRelative: "/" });
+
+export default function Landing() {
   return (
     <>
-      <header className="p-4 flex justify-end max-w-7xl mx-auto">
-        <ButtonSignin text="Login" />
-      </header>
-      <main>
-        <section className="flex flex-col items-center justify-center text-center gap-12 px-8 py-24">
-          <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
+      <Header /> 
+      <main
+        className="min-h-screen p-12 pb-24 text-center"
+        data-theme="dark"
+      >
+        <section className="max-w-xl mx-auto space-y-8">
+          <h1 className="text-3xl md:text-4xl font-extrabold">
+            Send and receive course material 🧑‍🏫
+          </h1>
 
-          <p className="text-lg opacity-80">
-            The start of your new startup... What are you gonna build?
+          <p className="text-lg leading-relaxed text-base-content/80">
+            Our platform lets you teach courses remotely across any device.
           </p>
 
-          <a
-            className="btn btn-primary"
-            href="https://shipfa.st/docs"
-            target="_blank"
-          >
-            Documentation & tutorials{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
+          <img
+            src="https://unsplash.com/illustrations/friendly-teacher-woman-stand-at-blackboard-explain-lesson-isolated-on-white-background-young-female-tutor-character-wear-formal-suit-teach-students-in-school-class-cartoon-people-vector-illustration-miVw6M4YEOk"
+            alt="Vegetables"
+            width={500}
+            height={250}
+            className="rounded-lg mx-auto"
+          />
 
-          <Link href="/blog" className="link link-hover text-sm">
-            Fancy a blog?
-          </Link>
+          <button className="btn btn-primary btn-wide">Get started</button>
         </section>
       </main>
     </>
